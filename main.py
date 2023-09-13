622a7a04db2d2a5bc7da5a39cbe7edfc995787ba
Cole Hicks <cole.t.hicks-1@ou.edu>
2023-09-13 13:45:26 -0500

    Added a main function to the file.

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ def list_product(list):
     for num in list:
         result *= num
     return total
+
+def main():
+    user_input = input("Enter a list of numbers separated by commas: ")
+    
+    list = [int(x) for x in user_input.split(',')]
+
+    sum = list_sum(list)
+    print("The sum is:", sum)
+
+    product = list_product(list)
+    print("The product is:", product)
